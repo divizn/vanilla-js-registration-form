e50f46733bd588d61d373db97d81b4dc98e1760c
divizn <hassanjaved186@gmail.com>
2022-10-01 15:18:33 +0100

    init

---
+++ CS2001-Week3-RegistrationValidation.js
@@ -0,0 +1,85 @@
+//Let's define some variables to represent the form data
+//At the beginning, let's assume the form is not valid
+var formValid=false;
+var username;
+var email;
+var password;
+var repPassword;
+var userType;
+var tosCheckBox;
+
+//Function to read the form
+function readForm(){
+    //ToDo: This function should read the form inputs, and assign their
+    //values to the variables defined above; See Lecture slide 46
+    //You can also get elements by ID: https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+    //You can get elements by tag name: https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName 
+    //Read username, email, password, repPassowrd, and tosCheckBox fields
+
+    //Example for creating the userType array from checkboxes
+    //Access the buyer and seller checkboxes via their IDs
+    //Create an empty userType array
+    //If buyer is checked, push "buyer" to userType
+    //If seller is checked, push "seller" to userType
+    var buyerCheckBox = document.getElementById("buyer");
+    var sellerCheckBox = document.getElementById("seller");
+    userType = [];
+    if(buyerCheckBox.checked)
+        userType.push(buyerCheckBox.value);
+
+    if(sellerCheckBox.checked)
+        userType.push(sellerCheckBox.value);
+}
+
+//Function to validate the form
+function validateForm() {
+    formValid = false;
+     //ToDo: Fill in the conditions to check the validity of the form.
+    //For example, you need to check if any of the text fields are empty
+    //If you have assigned the variables in readForm correctly, you can
+    //see their values for example, by using value for a text input, or checked
+    //for a checkbox
+    //You can also follow HTML5 validation here: https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+    //and see if you can use the validity field - for example, username.validity 
+    //to check if the form input is valid
+    if (false){/*ToDo: Add the correct condition to check no text field is empty*/
+            alert("Please fill in all text fields.");
+    } else if (false){/*ToDo: Add the correct condition to check email is of correct form*/
+        alert("Invalid e-mail address. Please enter your e-mail again.");
+    } else if (false){/*ToDo: Add the correct password condition*/
+        alert("Password is too short. Please select another password");
+    } else if(false) {/*ToDo: Check if passwords match*/
+        alert("Passwords do not match. Please retry");
+    } else if(userType.length === 0){
+        alert("Please check at least one checkbox to select being a seller or a buyer in the system.")
+    } else if (false){ /*ToDo: Add Condition to check Term and Conditions checked*/
+        alert("Please agree to the Terms and Conditions, and Privacy Policy.")
+    }else {
+        formValid = true;
+    }
+}
+
+//Function to write the Registration success on the page
+function createNewParagraph(content){
+    var text = document.createTextNode(content);
+    var paragraph = document.createElement("p");
+    paragraph.appendChild(text);
+
+    var element = document.getElementById("hiddenSection");
+    element.appendChild(paragraph);
+}
+
+//Function  to submit the form, this should be called by the Register button
+//on click
+function submitForm(){
+    readForm();
+    validateForm();
+    if (formValid){
+        var formText = username.value + " registered as: "
+                     + userType.join(" and ") 
+                     + ".";
+
+
+        createNewParagraph(formText);
+    }
+}
